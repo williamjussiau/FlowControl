fda9f7d5d5fc58e027407eb6a6304ef0b515d4cc
Mac <william.jussiau@gmail.com>
2025-01-21 01:10:10 +0100

    Reorganized fields + comments

--- cylinder/flowfield.py
+++ cylinder/flowfield.py
@@ -19,17 +19,21 @@ class FlowField:
 
 @dataclass
 class FlowFieldCollection:
-    ic: FlowField | None = None
+    # Base flow (full field)
     STEADY: FlowField | None = None
+    U0: dolfin.Function | None = None
+    P0: dolfin.Function | None = None
+    UP0: dolfin.Function | None = None
+    # Initial condition (pert field)
+    ic: FlowField | None = None
+    # Current and past solutions (pert field)
     u_: dolfin.Function | None = None
     p_: dolfin.Function | None = None
     up_: dolfin.Function | None = None
     u_n: dolfin.Function | None = None
     u_nn: dolfin.Function | None = None
     p_n: dolfin.Function | None = None
+    # Saved fields (full field)
     Usave: dolfin.Function | None = None
     Psave: dolfin.Function | None = None
     Usave_n: dolfin.Function | None = None
-    U0: dolfin.Function | None = None
-    P0: dolfin.Function | None = None
-    UP0: dolfin.Function | None = None
