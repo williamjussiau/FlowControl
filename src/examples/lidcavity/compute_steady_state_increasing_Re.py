6cee6d14412e644f3e80a71768d82d69f5500606
JamesAndrewKing <james.king@gmx.net>
2025-07-24 18:01:30 +0200

    Running forced simulations

--- src/examples/lidcavity/compute_steady_state_increasing_Re.py
+++ src/examples/lidcavity/compute_steady_state_increasing_Re.py
@@ -67,8 +67,8 @@ def main():
         xloc=0.1, yloc=0.1, radius=0.1, amplitude=0.1
     )
 
-    # Re_list = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 7500, 8000, Re_final]
     Re_list = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 7500, 8000, Re_final]
+    # Re_list = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 7500, Re_final]
     first_loop = True
     steady_state_filename_U0 = ""
     steady_state_filename_P0 = ""
@@ -109,7 +109,7 @@ def main():
         fs.compute_steady_state(
             method="picard",
             max_iter=10,
-            tol=3e-7,
+            tol=2e-7,
             u_ctrl=uctrl0,
             initial_guess=initial_guess,
         )
