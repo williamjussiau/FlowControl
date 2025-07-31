f1c0a4453ae3c033d05959ca4a26bfcdb706af11
JamesAndrewKing <james.king@gmx.net>
2025-07-31 19:44:28 +0200

    code to run cylinder

--- src/examples/cylinder/batch_run_cylinder.py
+++ src/examples/cylinder/batch_run_cylinder.py
@@ -1,16 +1,3 @@
-"""
-----------------------------------------------------------------------
-Flow over an open cavity
-Nondimensional incompressible Navier-Stokes equations
-Suggested Re=7500
-----------------------------------------------------------------------
-This file demonstrates the following possibilites:
-    - Initialize CavityFlowSolver object
-    - Compute steady-state
-    - Perform open-loop time simulation
-----------------------------------------------------------------------
-"""
-
 import logging
 import time
 from pathlib import Path
@@ -20,10 +7,11 @@ import numpy as np
 
 import flowcontrol.flowsolverparameters as flowsolverparameters
 import utils.utils_flowsolver as flu
-from examples.cavity.cavityflowsolver import CavityFlowSolver
-from flowcontrol.actuator import ActuatorForceGaussianV, ActuatorBCParabolicV
-from flowcontrol.sensor import SENSOR_TYPE, SensorHorizontalWallShear, SensorPoint
-from examples.cavity.compute_steady_state import Re
+from examples.cylinder.cylinderflowsolver import CylinderFlowSolver
+from flowcontrol.actuator import ActuatorBCParabolicV
+from flowcontrol.controller import Controller
+from flowcontrol.sensor import SENSOR_TYPE, SensorPoint
+from examples.cylinder.compute_steady_state import Re
 
 def run_lidcavity_with_ic(Re, xloc, yloc, radius, amplitude, save_dir, num_steps=100):
     # LOG
@@ -37,18 +25,13 @@ def run_lidcavity_with_ic(Re, xloc, yloc, radius, amplitude, save_dir, num_steps
 
     logger.info("Trying to instantiate FlowSolver...")
 
-    params_flow = flowsolverparameters.ParamFlow(Re=Re, uinf=1.0)
-    params_flow.user_data["L"] = 1.0
+    params_flow = flowsolverparameters.ParamFlow(Re=100, uinf=1.0)
     params_flow.user_data["D"] = 1.0
 
-    params_time = flowsolverparameters.ParamTime(num_steps=num_steps, dt=0.0004, Tstart=0.0)
+    params_time = flowsolverparameters.ParamTime(num_steps=num_steps, dt=0.005, Tstart=0.0)
 
-
-    # params_save = flowsolverparameters.ParamSave(
-    #     save_every=5, path_out=save_dir
-    # )
     params_save = flowsolverparameters.ParamSave(
-        save_every=200, path_out=save_dir
+        save_every=5, path_out=save_dir
     )
 
     params_solver = flowsolverparameters.ParamSolver(
@@ -56,45 +39,41 @@ def run_lidcavity_with_ic(Re, xloc, yloc, radius, amplitude, save_dir, num_steps
     )
 
     params_mesh = flowsolverparameters.ParamMesh(
-        meshpath=cwd / "data_input" / "cavity_coarse.xdmf"
+        meshpath=cwd / "data_input" / "O1.xdmf"
     )
-    params_mesh.user_data["xinf"] = 2.5
-    params_mesh.user_data["xinfa"] = -1.2
-    params_mesh.user_data["yinf"] = 0.5
-    params_mesh.user_data["x0ns_left"] = -0.4  # sf left from x0ns, ns right from x0ns
-    params_mesh.user_data["x0ns_right"] = 1.75  # ns left from x0ns, sf right from x0ns
+    params_mesh.user_data["xinf"] = 20
+    params_mesh.user_data["xinfa"] = -10
+    params_mesh.user_data["yinf"] = 10
 
     params_restart = flowsolverparameters.ParamRestart()
 
-    actuator_force = ActuatorForceGaussianV(
-        sigma=0.0849, position=np.array([-0.1, 0.02])
+    # duplicate actuators (1 top, 1 bottom) but assign same control input to each
+    angular_size_deg = 10
+    actuator_bc_1 = ActuatorBCParabolicV(
+        width=ActuatorBCParabolicV.angular_size_deg_to_width(
+            angular_size_deg, params_flow.user_data["D"] / 2
+        ),
+        position_x=0.0,
     )
-    # actuator_bc = ActuatorBCParabolicV(
-    #     width=7/20, position_x=np.array([-7/40])
-    # )
-    sensor_feedback = SensorHorizontalWallShear(
-        sensor_index=100,
-        x_sensor_left=1.0,
-        x_sensor_right=1.1,
-        y_sensor=0.0,
-        sensor_type=SENSOR_TYPE.OTHER,
+    actuator_bc_2 = ActuatorBCParabolicV(
+        width=ActuatorBCParabolicV.angular_size_deg_to_width(
+            angular_size_deg, params_flow.user_data["D"] / 2
+        ),
+        position_x=0.0,
     )
-    sensor_perf_1 = SensorPoint(
-        sensor_type=SENSOR_TYPE.U, position=np.array([0.1, 0.1])
-    )
+    sensor_feedback = SensorPoint(sensor_type=SENSOR_TYPE.V, position=np.array([3, 0]))
+    sensor_perf_1 = SensorPoint(sensor_type=SENSOR_TYPE.V, position=np.array([3.1, 1]))
+    sensor_perf_2 = SensorPoint(sensor_type=SENSOR_TYPE.V, position=np.array([3.1, -1]))
     params_control = flowsolverparameters.ParamControl(
-        sensor_list=[sensor_feedback, sensor_perf_1],
-        actuator_list=[actuator_force],
+        sensor_list=[sensor_feedback, sensor_perf_1, sensor_perf_2],
+        actuator_list=[actuator_bc_1, actuator_bc_2],
     )
 
-    # params_ic = flowsolverparameters.ParamIC(
-    #     xloc=2.0, yloc=0.0, radius=0.5, amplitude=1.0
-    # )
     params_ic = flowsolverparameters.ParamIC(
         xloc=xloc, yloc=yloc, radius=radius, amplitude=amplitude
     )
 
-    fs = CavityFlowSolver(
+    fs = CylinderFlowSolver(
         params_flow=params_flow,
         params_time=params_time,
         params_save=params_save,
@@ -125,25 +104,26 @@ def run_lidcavity_with_ic(Re, xloc, yloc, radius, amplitude, save_dir, num_steps
     fs.initialize_time_stepping(ic=None)  # or ic=dolfin.Function(fs.W)
 
     logger.info("Step several times")
+    Kss = Controller.from_file(file=cwd / "data_input" / "Kopt_reduced13.mat", x0=0)
+
     for _ in range(fs.params_time.num_steps):
         y_meas = flu.MpiUtils.mpi_broadcast(fs.y_meas)
-        # u_ctrl = [0.3 + 0.1 * y_meas[0]]
-        u_ctrl = [0.0 * y_meas[0]]
-        # u_ctrl = [np.sin(fs.t) + 0 * y_meas[0]]
-        fs.step(u_ctrl=u_ctrl)
+        u_ctrl = Kss.step(y=-y_meas[0], dt=fs.params_time.dt)
+        fs.step(u_ctrl=[u_ctrl[0], u_ctrl[0]])
+        # or
+        # fs.step(u_ctrl=np.repeat(u_ctrl, repeats=2, axis=0))
 
     flu.summarize_timings(fs, t000)
-    fs.write_timeseries()
-
     logger.info(fs.timeseries)
+    fs.write_timeseries()
 
     return
 
 
 def main():
 
-    base_dir = Path("/Users/jaking/Desktop/PhD/open_cavity")
-    parent_dir = base_dir / f"Re{Re}_stable"
+    base_dir = Path("/Users/jaking/Desktop/PhD/cylinder")
+    parent_dir = base_dir / f"Re{Re}_test"
     parent_dir.mkdir(parents=True, exist_ok=True)
 
     # x_vals = np.linspace(0.2, 0.8, 3)
@@ -153,7 +133,7 @@ def main():
     x_vals = [2.0]
     y_vals = [0.0]
     radius = 0.5
-    amplitude = 0.0
+    amplitude = 1.0
     num_steps = 1000
     count = 1
     for xloc in x_vals:
---
+++ src/examples/cylinder/compute_steady_state.py
@@ -0,0 +1,123 @@
+"""
+----------------------------------------------------------------------
+Flow past a cylinder
+Nondimensional incompressible Navier-Stokes equations
+Supercritical Hopf bifurcation at Re_c=46
+Suggested Re=100
+----------------------------------------------------------------------
+This file demonstrates the following possibilites:
+    - Initialize CylinderFlowSolver object
+    - Compute steady-state
+    - Load controller from file
+    - Perform closed-loop time simulation
+    - Restart simulation
+----------------------------------------------------------------------
+"""
+
+import logging
+import time
+from pathlib import Path
+
+import dolfin
+import numpy as np
+
+import flowcontrol.flowsolverparameters as flowsolverparameters
+import utils.utils_flowsolver as flu
+from examples.cylinder.cylinderflowsolver import CylinderFlowSolver
+from flowcontrol.actuator import ActuatorBCParabolicV
+from flowcontrol.controller import Controller
+from flowcontrol.sensor import SENSOR_TYPE, SensorPoint
+
+Re = 100
+
+def main():
+    # LOG
+    dolfin.set_log_level(dolfin.LogLevel.INFO)  # DEBUG TRACE PROGRESS INFO
+    logger = logging.getLogger(__name__)
+    FORMAT = "[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s]: %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+    t000 = time.time()
+    cwd = Path(__file__).parent
+
+    logger.info("Trying to instantiate FlowSolver...")
+
+    params_flow = flowsolverparameters.ParamFlow(Re=Re, uinf=1.0)
+    params_flow.user_data["D"] = 1.0
+
+    params_time = flowsolverparameters.ParamTime(num_steps=10, dt=0.005, Tstart=0.0)
+
+    params_save = flowsolverparameters.ParamSave(
+        save_every=5, path_out=cwd / "data_output"
+    )
+
+    params_solver = flowsolverparameters.ParamSolver(
+        throw_error=True, is_eq_nonlinear=True, shift=0.0
+    )
+
+    params_mesh = flowsolverparameters.ParamMesh(
+        meshpath=cwd / "data_input" / "O1.xdmf"
+    )
+    params_mesh.user_data["xinf"] = 20
+    params_mesh.user_data["xinfa"] = -10
+    params_mesh.user_data["yinf"] = 10
+
+    params_restart = flowsolverparameters.ParamRestart()
+
+    # duplicate actuators (1 top, 1 bottom) but assign same control input to each
+    angular_size_deg = 10
+    actuator_bc_1 = ActuatorBCParabolicV(
+        width=ActuatorBCParabolicV.angular_size_deg_to_width(
+            angular_size_deg, params_flow.user_data["D"] / 2
+        ),
+        position_x=0.0,
+    )
+    actuator_bc_2 = ActuatorBCParabolicV(
+        width=ActuatorBCParabolicV.angular_size_deg_to_width(
+            angular_size_deg, params_flow.user_data["D"] / 2
+        ),
+        position_x=0.0,
+    )
+    sensor_feedback = SensorPoint(sensor_type=SENSOR_TYPE.V, position=np.array([3, 0]))
+    sensor_perf_1 = SensorPoint(sensor_type=SENSOR_TYPE.V, position=np.array([3.1, 1]))
+    sensor_perf_2 = SensorPoint(sensor_type=SENSOR_TYPE.V, position=np.array([3.1, -1]))
+    params_control = flowsolverparameters.ParamControl(
+        sensor_list=[sensor_feedback, sensor_perf_1, sensor_perf_2],
+        actuator_list=[actuator_bc_1, actuator_bc_2],
+    )
+
+    params_ic = flowsolverparameters.ParamIC(
+        xloc=2.0, yloc=0.0, radius=0.5, amplitude=1.0
+    )
+
+    fs = CylinderFlowSolver(
+        params_flow=params_flow,
+        params_time=params_time,
+        params_save=params_save,
+        params_solver=params_solver,
+        params_mesh=params_mesh,
+        params_restart=params_restart,
+        params_control=params_control,
+        params_ic=params_ic,
+        verbose=5,
+    )
+
+    logger.info("__init__(): successful!")
+
+    logger.info("Exporting subdomains...")
+    flu.export_subdomains(
+        fs.mesh, fs.boundaries.subdomain, cwd / "data_output" / "subdomains.xdmf"
+    )
+
+    logger.info("Compute steady state...")
+    uctrl0 = [0.0, 0.0]
+    fs.compute_steady_state(method="picard", max_iter=3, tol=1e-7, u_ctrl=uctrl0)
+    fs.compute_steady_state(
+        method="newton", max_iter=25, u_ctrl=uctrl0, initial_guess=fs.fields.UP0
+    )
+    # Expected:
+    # Newton iteration 4: r (abs) = 6.901e-14 (tol = 1.000e-10) r (rel) = 1.109e-11 (tol = 1.000e-09)
+
+
+if __name__ == "__main__":
+    main()
